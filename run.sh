d268129bbdf536d450f140d3520705b2f2d9aa31
Filipe Borba <flpborba@protonmail.com>
2023-02-05 11:51:05 -0300

    Prepare test

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 for i in $(seq $1 $2)
 do
-    ./build/src/ddt-exec $i # warmup
     perf stat -x , -r ${3:=1} -e task-clock ./build/src/ddt-exec $i
 done
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "bf.h"
+#include <numeric>
 
 auto main(int argc, char* argv[]) -> int
 {
     auto field_deg = std::stoi(argv[1]);
-    Cached<Inverse>(field_deg).uniformity(RowLookupTag());
+    auto field_order = 1 << field_deg;
+
+    for (auto i = field_order - 1; i > 0; --i) {
+        auto m = 1 << (2 * field_deg);
+        if (std::gcd(i, m - 1) == 1) {
+            Cached<Dickson>(field_deg, i, make_elem(0x1)).uniformity(RowLookupTag());
+            return 0;
+        }
+    }
 }
