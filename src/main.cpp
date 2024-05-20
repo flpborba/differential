968c22eb9b276d4544feaaf3655226b423954ac4
Filipe Borba <flpborba@protonmail.com>
2024-05-20 18:35:43 -0300

    Prepare Dickson test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include "bf.h"
+#include "difference_map.h"
 
 auto main(int argc, char* argv[]) -> int
 {
-    auto field_deg = std::stoi(argv[1]);
-    Cached<Inverse>(field_deg).uniformity(RowLookupTag());
+    auto begin = 1;
+    auto end = 20;
+    auto deg = 19;
+    auto uniformity = deg - 1;
+    auto a = make_elem(0x01);
+
+    for (auto i = begin; i != end; ++i) {
+        auto dickson = Cached<Dickson>(i, deg, a);
+        auto uniformity = dickson.uniformity(RowLookupTag());
+
+        if (uniformity == deg - 1) {
+            std::cout << i << ": .\n";
+        } else {
+            std::cout << i << ": " << uniformity << "\n";
+        }
+    }
 }
